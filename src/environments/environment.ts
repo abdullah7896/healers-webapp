b45cc5fac5a97b990d65dec5464492a26a759e86
abdullah9114 <abdullah.qasim@bigentities.com>
2024-03-06 18:20:37 +0500

    commented lines

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const environment = {
-    production: false,
-    apiUrl: 'https://dev-api.fieldforce-ai.com/' // Example API URL for development
-};
+// export const environment = {
+//     production: false,
+//     apiUrl: 'https://dev-api.fieldforce-ai.com/' // Example API URL for development
+// };
